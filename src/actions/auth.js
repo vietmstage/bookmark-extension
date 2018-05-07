c24170d1791db7898a07150b358431b13bda2145
Nhok chien <chien@mstage.io>
2018-05-07 13:44:28 +0700

    [Update] change userkit token

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 export const login = (email, password) => {
   return axios({
     method: 'POST',
-    url: 'http://userkit-identity-stg.ap-southeast-1.elasticbeanstalk.com/v1/auth/signin',
+    url: 'https://userkit-identity.mstage.io/v1/auth/signin',
     headers: {
       'Content-Type': 'application/json',
-      'X-USERKIT-TOKEN': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjoiNTllMDNhYWEwYmY1Y2MxNTIwODJkY2E4In0.qgCtZ9FA7bPUCRpG4HR8ql0cF1tPssgzaJkMFjkac9U'
+      'X-USERKIT-TOKEN': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjoiNWFkODU4MjRiM2NlYzM0MTUzMDRhZWI2IiwiaWF0IjoxNTI0MTI5MTI2fQ.4HywQhdO-7LEEYcwrAsybLqBArgzHbD0sy2yScU2Rjk'
     },
     data: {
       email,
